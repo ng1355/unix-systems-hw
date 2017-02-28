d01e4d2ac08a38c7723d1b6250c7c1ee6ce5600d
ng1355 <ng1355@nyu.edu>
2017-02-27 23:41:46 -0500

    du works but counts redundant hardlinks, implimented pseudovector for hardlink counts

--- hw3/du.c
+++ hw3/du.c
@@ -1,7 +1,11 @@
 //du implimentation by Nikita Georgiou
 //Does not use ntfw
-#define _XOPEN_SOURCE
-#define _XOPEN_SOURCE_EXTENDED
+//ran out of time teehee
+//TODO: Impliment inode counting for hardlinks
+//Document my freaking VECTOR & actually make it work lol
+
+#define _XOPEN_SOURCE 500
+#define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
 #include <fcntl.h>
@@ -10,76 +14,152 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <string.h>
+#include <limits.h>
 
-char* PROGRAM_NAME;
+const char* PROGRAM_NAME; //For error printing
 
-int du(char* dirname);
-void printdirs(DIR* dir);
+typedef struct{
+	ino_t* inodes;
+	size_t capacity;
+	size_t size;
+} inode_list;
 
-static inline DIR* sopendir(char* dirname);
-static inline char* getpath(char* path);
+int du(char* fullpath, inode_list* inodes);
+
+int inode_exists(ino_t inode, ino_t* inodes);
+inode_list* new_inodelist(size_t capacity, size_t size);
+size_t find_inode(ino_t inode, inode_list* inodes);
+void add_inode(ino_t inode, inode_list* inodes);
+void delete_inodelist(inode_list* inodes);
+
+
+static inline void* smalloc(size_t size);
+static inline void* srealloc(void* ptr, size_t size);
+
 
 int main(int argc, char** argv) {
 	PROGRAM_NAME = argv[0];
+	
+	//Setting up inode vector to keep track of hardlinks
+	inode_list* inodes;
+	inodes = new_inodelist(10, 0);
 
-	if(argc == 1) du(".");
-	else if(argc == 2) du(argv[1]);
+	if(argc == 1) du(".", inodes);
+	else if(argc == 2) du(argv[1], inodes);
 	else{
 		fprintf(stderr, "Usage: %s [dirname]\n", argv[0]);
+		free(inodes);
 		exit(EXIT_FAILURE);
 	}
+	delete_inodelist(inodes);
 }
 
-int du(char* dirname) {
-	DIR* dir = sopendir(dirname);
-	struct dirent* currdir;
-	struct stat dirent_data;
-	char* fullpath = NULL;
-	int block_count = 0;
-	//walk directory tree
-	while((currdir = readdir(dir)) != NULL){
-	//lstat dirent 
-		fullpath = currdir->d_name; // getpath(currdir->d_name);
-		getpath(currdir->d_name);
-//		printf("d_name: %s fullpath: %s\n", currdir->d_name, fullpath);
-		if(lstat(currdir->d_name, &dirent_data) != 0){ 
-			puts("lstat error");
-			perror(PROGRAM_NAME);
+/* Traverses the directory tree and sums the how many 1024B blocks each file
+ * takes up. basepath can be either relative or absolute, and is at most 
+ * PATH_MAX. Recurses into subdirectories  to sum their disk usage. 
+ * Prints directories that have been summed and their corresponding size. 
+ * Returns number of blocks in a given directory. 
+ * TODO: cleanup, memory safety. :( */
+int du(char* basepath, inode_list* inodes){
+	DIR* dir;
+	struct dirent* entry;
+	struct stat entdata;
+	char new_path[PATH_MAX];
+	char* dname;
+	long int blockcount = 0;
+
+	if((dir = opendir(basepath)) == NULL){
+			fprintf(stderr, "%s: cannot read directory '%s'",
+				PROGRAM_NAME, basepath);
+			perror("");
+			return 0;
+	}
+
+	while((entry = readdir(dir)) != NULL){
+		dname = entry->d_name;
+
+		//Make sure dirent's not .. 
+		if(((strnlen(dname, 3) == 2) && (strncmp(dname, "..", 2) == 0)))
+			continue;
+
+		if(find_inode(entry->d_ino, inodes) != 0){ 
+			add_inode(entry->d_ino, inodes);
 			continue;
 		}
-		/*
-	//recurse if its a directory
-		if(S_ISDIR(dirent_data.st_mode)){} //block_count += du(fullpath);
-	//else accumulate blocksize
-		else block_count += dirent_data.st_blocks;
-		*/
-		block_count += dirent_data.st_blocks / 2;
+
+		//Append files/directory name to provided dir
+		sprintf(new_path, "%s/%s", basepath, dname);
+
+		if(lstat(new_path, &entdata) != 0){
+			fprintf(stderr, "%s: cannot read directory '%s'",
+				PROGRAM_NAME, new_path);
+			perror("");
+			continue;
+		}
+
+		if(S_ISDIR(entdata.st_mode)){
+			if(((strnlen(dname, 2) == 1) && (dname[0] == '.')))
+				blockcount += entdata.st_blocks / 2;
+			else
+				blockcount += du(new_path, inodes);
+		}
+		else blockcount += entdata.st_blocks / 2;
 	}
-	//print directory size 
-	printf("%d\t%s\n", block_count, fullpath);
-//	free(fullpath);
+
+	printf("%ld\t%s\n", blockcount, basepath);
 	closedir(dir);
-	return block_count;
+	return blockcount;
 }
 
-static inline DIR* sopendir(char* dirname){
-	DIR* dir;
-	if((dir = opendir(dirname)) == NULL){
+static inline void* smalloc(size_t size){
+	void* mem;
+	if((mem = malloc(size)) == NULL){
 		perror(PROGRAM_NAME);
 		exit(EXIT_FAILURE);
 	}
-	return dir;
+	return mem;
 }
 
-/* gets canonical path of filename
- * As of POSIX2008, passing NULL to realpath means path will point to a block
- * of memory at least PATH_MAX in size, older standard have implimentation
- * specific effects. Returns a pointer to the malloced path */
-static inline char* getpath(char* filename){
-	char* path;
-	if((path = realpath(filename, NULL)) == NULL){
+inode_list* new_inodelist(size_t capacity, size_t size){
+	inode_list* inodes;
+	inodes = (inode_list*) smalloc(sizeof(inode_list));
+	inodes->inodes = (ino_t*) smalloc(capacity * sizeof(ino_t));
+	inodes->capacity = capacity;
+	inodes->size = size;
+	return inodes;
+}
+
+size_t find_inode(ino_t inode, inode_list* inodes){
+	size_t loc = 0;
+	for(size_t i = 0; i < inodes->size; i++)
+		if(inodes->inodes[i] == inode){
+			loc = i;
+			break;
+		}
+	return loc;
+}
+
+void add_inode(ino_t inode, inode_list* inodes){
+	if(inodes->size == inodes->capacity){
+		inodes->inodes = srealloc(inodes->inodes, inodes->capacity * 2);
+		inodes->capacity *= 2;
+	}
+
+	inodes->inodes[inodes->size + 1] = inode;
+	inodes->size++;
+}
+
+void delete_inodelist(inode_list* inodes){
+	free(inodes->inodes);
+	free(inodes);
+}
+
+static inline void* srealloc(void* ptr, size_t size){
+	if((ptr = realloc(ptr, size)) == NULL){
 		perror(PROGRAM_NAME);
 		exit(EXIT_FAILURE);
 	}
-	return path;
+	return ptr;
 }
+
