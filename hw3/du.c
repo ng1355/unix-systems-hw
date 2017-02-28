3809caaa633091e3773e0aa6708a49d64a89b01a
ng1355 <ng1355@nyu.edu>
2017-02-28 00:56:33 -0500

    hardlinks work

--- hw3/du.c
+++ hw3/du.c
@@ -1,8 +1,8 @@
 //du implimentation by Nikita Georgiou
 //Does not use ntfw
 //ran out of time teehee
-//TODO: Impliment inode counting for hardlinks
-//Document my freaking VECTOR & actually make it work lol
+//i just did so much commenting omg, hardlink counts work!!!!!
+//TODO: cleanup 
 
 #define _XOPEN_SOURCE 500
 #define _POSIX_C_SOURCE 200809L
@@ -19,6 +19,10 @@
 
 const char* PROGRAM_NAME; //For error printing
 
+/* a simple vector to record encountered inodes
+ * will be used more like a map in implimentation.
+ * As such, there's no check for it, but its assumed each unique inode
+ * will have only one entry. */
 typedef struct{
 	ino_t* inodes;
 	size_t capacity;
@@ -29,14 +33,12 @@ int du(char* fullpath, inode_list* inodes);
 
 int inode_exists(ino_t inode, ino_t* inodes);
 inode_list* new_inodelist(size_t capacity, size_t size);
-size_t find_inode(ino_t inode, inode_list* inodes);
+size_t count_inode(ino_t inode, inode_list* inodes);
 void add_inode(ino_t inode, inode_list* inodes);
 void delete_inodelist(inode_list* inodes);
 
-
 static inline void* smalloc(size_t size);
 static inline void* srealloc(void* ptr, size_t size);
-
 
 int main(int argc, char** argv) {
 	PROGRAM_NAME = argv[0];
@@ -59,8 +61,8 @@ int main(int argc, char** argv) {
  * takes up. basepath can be either relative or absolute, and is at most 
  * PATH_MAX. Recurses into subdirectories  to sum their disk usage. 
  * Prints directories that have been summed and their corresponding size. 
- * Returns number of blocks in a given directory. 
- * TODO: cleanup, memory safety. :( */
+ * Returns number of blocks in a given directory for recusion. 
+ * TODO: cleanup, stack memory safety. :( */
 int du(char* basepath, inode_list* inodes){
 	DIR* dir;
 	struct dirent* entry;
@@ -68,36 +70,40 @@ int du(char* basepath, inode_list* inodes){
 	char new_path[PATH_MAX];
 	char* dname;
 	long int blockcount = 0;
+	size_t inode_exists = 0;
 
 	if((dir = opendir(basepath)) == NULL){
-			fprintf(stderr, "%s: cannot read directory '%s'",
-				PROGRAM_NAME, basepath);
-			perror("");
+			perror(PROGRAM_NAME);
 			return 0;
 	}
 
 	while((entry = readdir(dir)) != NULL){
 		dname = entry->d_name;
 
-		//Make sure dirent's not .. 
+		//Make sure dirent's not ".." 
 		if(((strnlen(dname, 3) == 2) && (strncmp(dname, "..", 2) == 0)))
 			continue;
 
-		if(find_inode(entry->d_ino, inodes) != 0){ 
-			add_inode(entry->d_ino, inodes);
+		/*If there's a record of the inode in the inode list, 
+		 * skip the loop. Otherwise, add it to the list if it's not 
+		 * already there. */ 
+		if((inode_exists = count_inode(entry->d_ino, inodes)) != 0)
 			continue;
-		}
+
+		if(inode_exists != 1) add_inode(entry->d_ino, inodes);
 
 		//Append files/directory name to provided dir
+		//TODO: Some absolute paths Have extraneous "/" at beginning
+		//harmless, but not visually pleasing
 		sprintf(new_path, "%s/%s", basepath, dname);
 
 		if(lstat(new_path, &entdata) != 0){
-			fprintf(stderr, "%s: cannot read directory '%s'",
-				PROGRAM_NAME, new_path);
-			perror("");
+			perror(PROGRAM_NAME);
 			continue;
 		}
 
+		//if its a directory but not ".", recurse into it
+		//giving the folder relative to the cwd.
 		if(S_ISDIR(entdata.st_mode)){
 			if(((strnlen(dname, 2) == 1) && (dname[0] == '.')))
 				blockcount += entdata.st_blocks / 2;
@@ -112,6 +118,7 @@ int du(char* basepath, inode_list* inodes){
 	return blockcount;
 }
 
+//mallocs or dies. im getting lazier with the names lol
 static inline void* smalloc(size_t size){
 	void* mem;
 	if((mem = malloc(size)) == NULL){
@@ -121,6 +128,9 @@ static inline void* smalloc(size_t size){
 	return mem;
 }
 
+//Essentially a constructor for the inode list
+//dynamic array for to store inode numbers, as well as struct itself
+//returns pointer to inode list struct 
 inode_list* new_inodelist(size_t capacity, size_t size){
 	inode_list* inodes;
 	inodes = (inode_list*) smalloc(sizeof(inode_list));
@@ -130,31 +140,32 @@ inode_list* new_inodelist(size_t capacity, size_t size){
 	return inodes;
 }
 
-size_t find_inode(ino_t inode, inode_list* inodes){
-	size_t loc = 0;
+//Counts the number of inode numbers matching "inode", shouldnt be more than 1
+//returns that count
+size_t count_inode(ino_t inode, inode_list* inodes){
+	size_t count = 0;
 	for(size_t i = 0; i < inodes->size; i++)
-		if(inodes->inodes[i] == inode){
-			loc = i;
-			break;
-		}
-	return loc;
+		if(inodes->inodes[i] == inode) count++;
+	return count;
 }
 
+//Adds a new inode to the inode list's list. If the list runs out of space,
+//double capacity a la vector (well, not quite) with realloc. 
 void add_inode(ino_t inode, inode_list* inodes){
 	if(inodes->size == inodes->capacity){
-		inodes->inodes = srealloc(inodes->inodes, inodes->capacity * 2);
+		inodes->inodes = srealloc(inodes->inodes, sizeof(ino_t) * inodes->capacity * 2);
 		inodes->capacity *= 2;
 	}
-
-	inodes->inodes[inodes->size + 1] = inode;
-	inodes->size++;
+	inodes->inodes[inodes->size += 1] = inode;
 }
 
+//destructor 
 void delete_inodelist(inode_list* inodes){
 	free(inodes->inodes);
 	free(inodes);
 }
 
+//reallocs or dies trying 
 static inline void* srealloc(void* ptr, size_t size){
 	if((ptr = realloc(ptr, size)) == NULL){
 		perror(PROGRAM_NAME);
@@ -162,4 +173,3 @@ static inline void* srealloc(void* ptr, size_t size){
 	}
 	return ptr;
 }
-
