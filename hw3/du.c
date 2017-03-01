07b8a8085e73976fb4df2d8dc1e5aac58f71d883
ng1355 <ng1355@nyu.edu>
2017-03-01 02:23:50 -0500

    fixed extraneous slash, refactored, added more error checking & documentation

--- hw3/du.c
+++ hw3/du.c
@@ -1,22 +1,20 @@
 //du implimentation by Nikita Georgiou
 //Does not use ntfw
-//ran out of time teehee
-//i just did so much commenting omg, hardlink counts work!!!!!
-//compiled & works on arch linux
-//TODO: cleanup, find out why segfault happens on mac
 
 #define _XOPEN_SOURCE 500
 #define _POSIX_C_SOURCE 200809L
 
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <sys/stat.h>
-#include <sys/types.h>
 #include <unistd.h>
 #include <string.h>
 #include <limits.h>
+#include <errno.h>
+
+//meant to determine if the length of str1 + str2 exceeds max (ie PATH_MAX)
+#define IS_OVERFLOW(str1, str2, max) (((str1) + (str2)) > (max))
 
 const char* PROGRAM_NAME; //For error printing
 
@@ -32,9 +30,8 @@ typedef struct{
 
 int du(char* fullpath, inode_list* inodes);
 
-int inode_exists(ino_t inode, ino_t* inodes);
-inode_list* new_inodelist(size_t capacity, size_t size);
-size_t count_inode(ino_t inode, inode_list* inodes);
+inode_list* new_inodelist(size_t capacity);
+int inode_exists(ino_t inode, inode_list* inodes);
 void add_inode(ino_t inode, inode_list* inodes);
 void delete_inodelist(inode_list* inodes);
 
@@ -45,17 +42,21 @@ int main(int argc, char** argv) {
 	PROGRAM_NAME = argv[0];
 	
 	//Setting up inode vector to keep track of hardlinks
-	inode_list* inodes;
-	inodes = new_inodelist(10, 0);
+	inode_list* inodes = new_inodelist(10);
 
 	if(argc == 1) du(".", inodes);
-	else if(argc == 2) du(argv[1], inodes);
+	else if(argc == 2){
+		if(strnlen(argv[1], PATH_MAX) + 1 > PATH_MAX){
+			fprintf(stderr, "%s: path too long\n", PROGRAM_NAME);
+			exit(EXIT_FAILURE);
+		}
+	       	du(argv[1], inodes);
+	}
 	else{
 		fprintf(stderr, "Usage: %s [dirname]\n", argv[0]);
 		free(inodes);
 		exit(EXIT_FAILURE);
 	}
-	puts("dont executing");
 	delete_inodelist(inodes);
 }
 
@@ -63,8 +64,7 @@ int main(int argc, char** argv) {
  * takes up. basepath can be either relative or absolute, and is at most 
  * PATH_MAX. Recurses into subdirectories  to sum their disk usage. 
  * Prints directories that have been summed and their corresponding size. 
- * Returns number of blocks in a given directory for recusion. 
- * TODO: cleanup, stack memory safety. :( */
+ * Returns number of blocks in a given directory for recusion. */
 int du(char* basepath, inode_list* inodes){
 	DIR* dir;
 	struct dirent* entry;
@@ -72,14 +72,23 @@ int du(char* basepath, inode_list* inodes){
 	char new_path[PATH_MAX];
 	char* dname;
 	long int blockcount = 0;
-	size_t inode_exists = 0;
 
 	if((dir = opendir(basepath)) == NULL){
-			perror(PROGRAM_NAME);
-			return 0;
+		fprintf(stderr, "%s: couldn't open directory: '%s'; %s\n",
+			PROGRAM_NAME, basepath, strerror(errno));
+		return 0;
 	}
 
 	while((entry = readdir(dir)) != NULL){
+		/* dont waste time with directories that exceed PATH_MAX
+		 * as to avoid smashing new_path 
+		 * doesnt abort like in main */
+		if(IS_OVERFLOW(strnlen(basepath, PATH_MAX) + 1, 
+				strnlen(basepath, PATH_MAX) + 1, PATH_MAX)){
+			fprintf(stderr, "%s: path too long\n", PROGRAM_NAME);
+			continue;
+		}
+
 		dname = entry->d_name;
 
 		//Make sure dirent's not ".." 
@@ -89,18 +98,20 @@ int du(char* basepath, inode_list* inodes){
 		/*If there's a record of the inode in the inode list, 
 		 * skip the loop. Otherwise, add it to the list if it's not 
 		 * already there. */ 
-		if((inode_exists = count_inode(entry->d_ino, inodes)) != 0)
-			continue;
-
-		if(inode_exists != 1) add_inode(entry->d_ino, inodes);
+		if(inode_exists(entry->d_ino, inodes)) continue;
+		else add_inode(entry->d_ino, inodes);
 
 		//Append files/directory name to provided dir
-		//TODO: Some absolute paths Have extraneous "/" at beginning
-		//harmless, but not visually pleasing
-		sprintf(new_path, "%s/%s", basepath, dname);
+		//check necessary for edgecase where basepath is root ('/')
+		//else "%s/%s" adds extraneous / to new_path
+		if((strnlen(basepath, 2) == 1) && (basepath[0] == '/'))
+			snprintf(new_path, PATH_MAX, "%s%s", basepath, dname);
+		else 
+			snprintf(new_path, PATH_MAX, "%s/%s", basepath, dname);
 
 		if(lstat(new_path, &entdata) != 0){
-			perror(PROGRAM_NAME);
+			fprintf(stderr, "%s: couldn't stat path: %s\n",
+				PROGRAM_NAME, new_path);
 			continue;
 		}
 
@@ -120,43 +131,33 @@ int du(char* basepath, inode_list* inodes){
 	return blockcount;
 }
 
-//mallocs or dies. im getting lazier with the names lol
-static inline void* smalloc(size_t size){
-	void* mem;
-	if((mem = malloc(size)) == NULL){
-		perror(PROGRAM_NAME);
-		exit(EXIT_FAILURE);
-	}
-	return mem;
-}
-
 //Essentially a constructor for the inode list
 //dynamic array for to store inode numbers, as well as struct itself
 //returns pointer to inode list struct 
-inode_list* new_inodelist(size_t capacity, size_t size){
+inode_list* new_inodelist(size_t capacity){
 	inode_list* inodes;
-	inodes = (inode_list*) smalloc(sizeof(inode_list));
-	inodes->inodes = (ino_t*) smalloc(capacity * sizeof(ino_t));
+	inodes = smalloc(sizeof(inode_list));
+	inodes->inodes = smalloc(capacity * sizeof(ino_t));
 	inodes->capacity = capacity;
-	inodes->size = size;
+	inodes->size = 0;
 	return inodes;
 }
 
 //Counts the number of inode numbers matching "inode", shouldnt be more than 1
 //returns that count
-size_t count_inode(ino_t inode, inode_list* inodes){
-	size_t count = 0;
+int inode_exists(ino_t inode, inode_list* inodes){
 	for(size_t i = 0; i < inodes->size; i++)
-		if(inodes->inodes[i] == inode) count++;
-	return count;
+		if(inodes->inodes[i] == inode) return 1;
+	return 0;
 }
 
 //Adds a new inode to the inode list's list. If the list runs out of space,
 //double capacity a la vector (well, not quite) with realloc. 
 void add_inode(ino_t inode, inode_list* inodes){
 	if(inodes->size == inodes->capacity){
-		inodes->inodes = srealloc(inodes->inodes, sizeof(ino_t) * inodes->capacity * 2);
-		inodes->capacity *= 2;
+		inodes->inodes = srealloc(inodes->inodes, 
+				 sizeof(ino_t) * inodes->capacity * 1.5);
+		inodes->capacity *= 1.5;
 	}
 	inodes->inodes[inodes->size += 1] = inode;
 }
@@ -165,6 +166,16 @@ void add_inode(ino_t inode, inode_list* inodes){
 void delete_inodelist(inode_list* inodes){
 	free(inodes->inodes);
 	free(inodes);
+}
+
+//mallocs or dies. im getting lazier with the names lol
+static inline void* smalloc(size_t size){
+	void* mem;
+	if((mem = malloc(size)) == NULL){
+		perror(PROGRAM_NAME);
+		exit(EXIT_FAILURE);
+	}
+	return mem;
 }
 
 //reallocs or dies trying 
